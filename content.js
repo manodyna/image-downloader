ccb4ae6a7f56f9d232c94d100416799656afedec
manodyna <77208955+manodyna@users.noreply.github.com>
2021-02-24 11:15:12 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,19 +1,23 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+// gets all images on the page if the pugin is opened
+chrome.runtime.onMessage.addLisener((request, sender, sendResponse) => {
     if (request.action === "get_images") {
-        let formatted_images = [];
+        let formattedImages = [];
 
+        
+//         gets the data from the images
         let images = $('img').map((i, img) => {
             if($(img).data('src')) {
-                formatted_images.push({
+                formattedImages.push({
                     src: $(img).data('src')
                 })
             } else if($(img).src) {
-                formatted_images.push({
+                formattedImages.push({
                     src: $(img).src
                 })
             }
         })
 
-        sendResponse(formatted_images);
+//         sending the formatted images
+        sendResponse(formattedImages);
     }
 })
