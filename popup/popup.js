19fd8765a21f4405625b122578f493d95f234b90
manodyna <77208955+manodyna@users.noreply.github.com>
2021-02-24 11:19:11 +0530

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,6 @@
 var images = [];
 
+// querying for active tab and adding images from active tab to gallery class
 chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {action: "get_images"}, response => {
         $('.gallery').html('');
@@ -11,6 +12,7 @@ chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     })
 })
 
+// downloading images on button click and sending a runtime message to chrome
 $(document).on('click', '#download_all', (e) => {
     console.log("Downloading all");
     chrome.runtime.sendMessage({action: "download", data: images}, res => {
